f5d97f6a875b2e3d6eb8af779f3077252a4901db
Shivarsh <135205670+Shivarsh@users.noreply.github.com>
2023-12-23 09:29:00 +0530

    Update settings.py

--- furni/furni/settings.py
+++ furni/furni/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-6#-4+f7-v-uwl5^nziq%0@&)8ump2ew6u2k7vmz#1_a1s8srnj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS=['127.0.0.1','pythonanywhere.com','localhost']
+ALLOWED_HOSTS=['127.0.0.1','.pythonanywhere.com','localhost']
 
 ALLOWED_HOSTS = []
 
